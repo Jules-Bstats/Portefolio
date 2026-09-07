0598c9c924ed5b2f0c2b2d918c1350b052240cf7
Jules <jules.bouscaud2@gmail.com>
2026-09-07 18:21:31 +0200

    modif test

--- portefolio.py
+++ portefolio.py
@@ -49,7 +49,7 @@ PROJECTS = {
             },
             "Analyse2": {
                     "title": "Analyse",
-                    "date": "Septembre 2025",
+                    "date": "Septembre 2026",
                     "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                     "image": "img/projet4.jpg",
                     "skills": ["r", "binome"],
