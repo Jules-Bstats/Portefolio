ba4d7bf0c56cb38cf5eec3a2a1b94cc9cd64952c
Jules <jules.bouscaud2@gmail.com>
2026-09-18 17:17:35 +0200

    Mise à jour du site + ajout contenu + maj image

--- portefolio.py
+++ portefolio.py
@@ -3,6 +3,27 @@ from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 PROJECTS = {
+
+# accident de la route -----------------------------------------------------
+    "accident routier": {
+                "title": "Analyse économétrique du taux d'accidents de la route en France",
+                "date": "Décembre 2025",
+                "summary": """
+                    blabla 
+                    """,
+                    "summary_bottom":"""blabla """,
+                "image": "img/projet6.jpg",
+                "skills": ["r", "binome"],
+                "comment": """L'auto-critique met toutefois en lumière la complexité inhérente 
+                    l'imputation de telles bases issues d'enquêtes, particulièrement lorsque la majorité des variables sont qualitatives et possèdent de multiples modalirés.""",
+                "data_origin": "INSEE",
+                "data_link": "https://www.insee.fr/fr/statistiques",
+                "project_file": "docs/nuitees_projet.pdf",
+                "synthesis_file": "docs/nuitees_synthese.pdf"
+            },
+
+# prévision des nuitées  -----------------------------------------------------------------------------------------  
+    
     "nuitees": {
         "title": "Prévision de la fréquentation touristique en France",
         "date": "Juin 2026",
@@ -34,10 +55,12 @@ PROJECTS = {
                 Ce mémoire m'a permis synthétiser mes pensées pour faire comprendre mon travail à toute personne, même non spécialiste. 
                 """,
         "data_origin": "INSEE, Google Trends",
-        "data_link": "https://github.com/Jules-Bstats/memoire/blob/main/data_memoire.csv"
+        "data_link": "https://github.com/Jules-Bstats/memoire/blob/main/data_memoire.csv",
+                "project_file": "docs/nuitees_projet.pdf",
+                "synthesis_file": "docs/nuitees_synthese.pdf"
     },
 
-
+# POWER BI -----------------------------------------------------------------------------------------
     "dashboard": {
         "title": "Dashboard Power BI",
         "date": "Mars 2025",
@@ -46,25 +69,37 @@ PROJECTS = {
         "skills": ["powerbi", "solo"],
         "comment": "Lorem ipsum dolor sit amet. Autoévaluation : bonne prise en main de Power BI, à approfondir sur les mesures DAX complexes.",
         "data_origin": "Kaggle",
-        "data_link": "https://www.kaggle.com"
+        "data_link": "https://www.kaggle.com",
+                "project_file": "docs/nuitees_projet.pdf",
+                "synthesis_file": "docs/nuitees_synthese.pdf"
     },
 
-
+# BIOSTAT -----------------------------------------------------------------------------------------
 
 
     "Nettoyage": {
             "title": "Nettoyage de données ",
-            "date": "Septembre 2025",
-            "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            "image": "img/projet1.jpg",
+            "date": "Novembre 2025",
+            "summary": """
+                Dans le cadre d’un projet de biostatistique réalisé en binôme sur une enquête d'Etalab, l’objectif était d’analyser l’attractivité des métiers du numérique dans la fonction publique et de 
+                maîtriser le traitement des valeurs manquantes. Pour ce faire, la base initiale a été enrichie et nettoyée sous RStudio, passant de 13 à 28 variables grâce à l'ajout de nouvelles dimensions quantitatives et qualitatives. 
+                Une perte de données de 15 % a ensuite été simulée artificiellement sur plusieurs variables clés afin de tester des stratégies rigoureuses d'imputation. Le projet mobilise une panoplie de techniques comparées, allant des 
+                régressions linéaires et logistiques aux approches par la médiane selon le genre et l'expérience, en passant par l'algorithme MICE (Predictive Mean Matching). 
+                """,
+                "summary_bottom":"""Les principaux résultats démontrent que la méthode MICE s'avère la 
+                plus performante pour estimer les variables quantitatives comme les salaires, tandis que l'approche par tendances observées fonctionne mieux pour les qualitatives.  """,
+            "image": "img/projet3.jpg",
             "skills": ["r", "binome"],
-            "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Autoévaluation : projet abouti, bonne maîtrise des modèles SARIMA, marge d'amélioration sur l'automatisation du pipeline.",
+            "comment": """L'auto-critique met toutefois en lumière la complexité inhérente 
+                l'imputation de telles bases issues d'enquêtes, particulièrement lorsque la majorité des variables sont qualitatives et possèdent de multiples modalirés.""",
             "data_origin": "INSEE",
-            "data_link": "https://www.insee.fr/fr/statistiques"
+            "data_link": "https://www.insee.fr/fr/statistiques",
+                "project_file": "docs/nuitees_projet.pdf",
+                "synthesis_file": "docs/nuitees_synthese.pdf"
         },
 
 
-
+# PREVISION BROCOLI -----------------------------------------------------------------------------------------
         "brocolis": {
                 "title": "Prévision du prix du brocoli aux USA",
                 "date": "Septembre 2025",
@@ -73,22 +108,44 @@ PROJECTS = {
                 "skills": ["r", "binome"],
                 "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Autoévaluation : projet abouti, bonne maîtrise des modèles SARIMA, marge d'amélioration sur l'automatisation du pipeline.",
                 "data_origin": "FED",
-                "data_link": "https://www.insee.fr/fr/statistiques"
+                "data_link": "https://www.insee.fr/fr/statistiques",
+                "project_file": "docs/nuitees_projet.pdf",
+                "synthesis_file": "docs/nuitees_synthese.pdf"
             },
 
 
-
+# ANALYSE EXPLORATOIRE  -----------------------------------------------------------------------------------------
             
-            "Analyse2": {
-                    "title": "Analyse",
-                    "date": "Septembre 2026",
+            "Analyse  de donnees": {
+                    "title": """Analyse des inégalités entre les pays du monde à l'aide  d’indicateurs socio-économiques,
+                                démographiques et sanitaires""",
+                    "date": "Février 2026",
                     "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                     "image": "img/projet4.jpg",
                     "skills": ["r", "binome"],
                     "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Autoévaluation : projet abouti, bonne maîtrise des modèles SARIMA, marge d'amélioration sur l'automatisation du pipeline.",
                     "data_origin": "INSEE",
-                    "data_link": "https://www.insee.fr/fr/statistiques"
-                }
+                    "data_link": "https://www.insee.fr/fr/statistiques",
+                    "project_file": "docs/nuitees_projet.pdf",
+                    "synthesis_file": "docs/nuitees_synthese.pdf"
+                    
+                },
+
+
+# R SHINY  -----------------------------------------------------------------------------------------
+
+            "viz memoire": {
+                        "title": "Application R-Shiny : visualisation des prévisions de fréquentation touristique en France",
+                        "date": "Juin2026",
+                        "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                        "image": "img/projet7.jpg",
+                        "skills": ["r-shiny", "data-viz"],
+                        "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Autoévaluation : projet abouti, bonne maîtrise des modèles SARIMA, marge d'amélioration sur l'automatisation du pipeline.",
+                        "data_origin": "INSEE",
+                        "data_link": "https://www.insee.fr/fr/statistiques",
+                        "project_file": "docs/nuitees_projet.pdf",
+                        "synthesis_file": "docs/nuitees_synthese.pdf"
+                                }
 }
 
 @app.route("/")
